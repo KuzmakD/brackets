86624944018f7141d775574453f64441cd110872
Dmitry Korzun <korzundm7@gmail.com>
2022-02-01 23:40:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const openingSymbols = bracketsConfig.flat().filter((elem, index) => index % 2 == 0);
+  const closingSymbols = bracketsConfig.flat().filter((elem, index) => index % 2 != 0);
+  
+  //console.log(openingSymbols, closingSymbols);
+  for (const char of str) {
+    if (openingSymbols.includes(char)) { 
+      stack.push(char); 
+    } else if (closingSymbols.includes(char)) {
+        if (openingSymbols[closingSymbols.indexOf(char)] !== stack.pop()) { 
+          return false; 
+        }
+      }
+  }
+  return stack.length === 0;
 }
