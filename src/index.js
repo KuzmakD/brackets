644d065a4fba1727d650713c501f75067613627a
Dmitry Korzun <korzundm7@gmail.com>
2022-02-02 12:31:56 +0300

    Update solution 22.02.02

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-module.exports = function check(str, bracketsConfig) {
+module.exports = function check (str, bracketsConfig) {
   const stack = [];
   const openingSymbols = bracketsConfig.flat().filter((elem, index) => index % 2 == 0);
   const closingSymbols = bracketsConfig.flat().filter((elem, index) => index % 2 != 0);
-  
-  //console.log(openingSymbols, closingSymbols);
-  for (const char of str) {
-    if (openingSymbols.includes(char)) { 
-      stack.push(char); 
-    } else if (closingSymbols.includes(char)) {
-        if (openingSymbols[closingSymbols.indexOf(char)] !== stack.pop()) { 
-          return false; 
+
+  for (let i = 0; i < str.length; i++) { 
+    if (openingSymbols.includes(str[i]) && closingSymbols.includes(str[i])) {
+      if (str[i] != stack[stack.length - 1]) { stack.push(str[i]); } else { stack.pop(); }
+    } else 
+      if (openingSymbols.includes(str[i])) { stack.push(str[i]); } else 
+        if (closingSymbols.includes(str[i])) {
+          if (openingSymbols[closingSymbols.indexOf(str[i])] != stack.pop()) { return false; }
         }
-      }
   }
+
   return stack.length === 0;
 }
